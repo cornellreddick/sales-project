9f5aa66bf3e2077a9162d85f47da52d785a3079f
Cornell Reddick <cornellreddick@outlook.com>
2022-08-17 10:37:23 -0400

    Added a list of SalesPerson

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  // creating a array of objects for sample data
+  salesPersonList: SalesPerson [] = [
+    new SalesPerson("Maurice", "Jackson", "maurice.jackson@gmail.com", 50000),
+    new SalesPerson("Sam", "Williams", "sam.williams@gmail.com", 70000),
+    new SalesPerson("Tina", "Trackson", "tina.trackson@gmail.com", 90000),
+    new SalesPerson("Larry", "Jackson", "larry.jackson@gmail.com", 30000),
+    new SalesPerson("Ray", "Reid", "ray.reid@gmail.com", 10000)
+  ]
 
   constructor() { }
 
